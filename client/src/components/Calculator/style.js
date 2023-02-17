1d2175c64a9465f0176ed91540d101d413927463
shimeeuisuk <euisuk95@gmail.com>
2023-02-17 11:45:30 +0900

    [Style] calulator 컴포넌트 css 추가

---
+++ client/src/components/Calculator/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container=styled.div`
+width: 100%;
+display:flex;
+flex-direction: column;
+align-items: center;
+padding-top: 100px;
+gap: 50px;
+`
+
+const S = {
+    Container
+}
+
+export default S
